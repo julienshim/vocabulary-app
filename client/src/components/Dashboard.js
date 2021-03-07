7cecdc426013a9539c80bf10f853ad46afad5bed
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-06 22:33:25 -0800

    dashboard init

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import { getAccessToken } from '../accessToken';
 
 const Dashboard = (props) => {
-  const { setAuth } = props;
+  const [name, setName] = useState('');
+
+  const getProfile = async () => {
+    try {
+      const accessToken = getAccessToken();
+      const response = await fetch('http://localhost:5000/dashboard', {
+        method: 'GET',
+        headers: {
+          Authorization: `bearer ${accessToken}`,
+        },
+      });
+      const parseRes = await response.json();
+      setName(parseRes.user_name);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <Fragment>
       <h1>Dashboard</h1>
-      <button type="button" onClick={() => setAuth(false)}>
+      <h2>Welcome {name}</h2>
+      {/* <button type="button" onClick={() => setAuth(false)}>
         Logout
-      </button>
+      </button> */}
     </Fragment>
   );
 };
