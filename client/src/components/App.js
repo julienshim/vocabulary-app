c27fc32b9387c7ccd9687f1e80b93b694cb0246d
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-12 23:24:45 -0800

    parseRes json

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Routes from './Routes';
 import { setAccessToken } from '../accessToken';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const renewAccessToken = async () => {
     try {
       const response = await fetch('http://localhost:5000/renewAccessToken', {
         method: 'POST',
         credentials: 'include',
       });
-      const res = await response;
-      setAccessToken(res.accessToken);
+      const parseRes = await response.json();
+      setAccessToken(parseRes.accessToken);
+      setIsLoading(false);
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error.message);
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     renewAccessToken();
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return <Routes />;
 };
