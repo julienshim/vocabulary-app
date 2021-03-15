609c1567c4b0e1d2bc486f91b2fd979516aa8e0c
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-14 20:45:14 -0700

    incorporate private route

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,28 +1,30 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import PrivateRoute from '../pages/PrivateRoute';
 
-import Dashboard from './Dashboard';
-import Login from './Login';
-import Register from './Register';
-
-function App() {
-  // eslint-disable-next-line no-unused-vars
-
+const Routes = (props) => {
+  // eslint-disable-next-line no-console
   const routes = [
     {
       path: '/login',
       exact: true,
+      isPrivate: false,
       render: () => <Login />,
     },
     {
       path: '/register',
       exact: true,
+      isPrivate: false,
       render: () => <Register />,
     },
     {
       path: '/dashboard',
       exact: true,
+      isPrivate: true,
       render: () => <Dashboard />,
     },
   ];
@@ -32,19 +34,29 @@ function App() {
       <Router>
         <div className="container">
           <Switch>
-            {routes.map((route, index) => (
-              <Route
-                key={`route-${index * Date.now()}`}
-                path={route.path}
-                exact={route.exact}
-                render={route.render}
-              />
-            ))}
+            {routes.map((route, index) => {
+              return route.isPrivate ? (
+                <PrivateRoute
+                  key={`route-${index * Date.now()}`}
+                  path={route.path}
+                  exact={route.exact}
+                  render={route.render}
+                />
+              ) : (
+                <Route
+                  key={`route-${index * Date.now()}`}
+                  path={route.path}
+                  exact={route.exact}
+                  render={route.render}
+                />
+              );
+            })}
+            ;
           </Switch>
         </div>
       </Router>
     </Fragment>
   );
-}
+};
 
-export default App;
+export default Routes;
