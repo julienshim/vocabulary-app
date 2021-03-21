5d663b89af8fb55cfdccf3285e40f5628c656c96
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-20 19:18:01 -0700

    user context provider

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,13 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from './Navigation';
 import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import VocabularyList from '../pages/VocabularyList';
 import PrivateRoute from '../pages/PrivateRoute';
+import UserContext from '../context/user-context';
+import useGetUser from '../hooks/useGetUser';
 
-const Routes = (props) => {
-  // eslint-disable-next-line no-console
+const Routes = () => {
+  const { isLoading, user } = useGetUser();
+
   const routes = [
     {
       path: '/login',
@@ -27,32 +32,45 @@ const Routes = (props) => {
       isPrivate: true,
       render: () => <Dashboard />,
     },
+    {
+      path: '/cards',
+      exact: true,
+      isPrivate: true,
+      render: () => <VocabularyList />,
+    },
   ];
+
+  if (isLoading) {
+    return <div>Loading Routes...</div>;
+  }
 
   return (
     <Fragment>
       <Router>
         <div className="container">
-          <Switch>
-            {routes.map((route, index) => {
-              return route.isPrivate ? (
-                <PrivateRoute
-                  key={`route-${index * Date.now()}`}
-                  path={route.path}
-                  exact={route.exact}
-                  render={route.render}
-                />
-              ) : (
-                <Route
-                  key={`route-${index * Date.now()}`}
-                  path={route.path}
-                  exact={route.exact}
-                  render={route.render}
-                />
-              );
-            })}
-            ;
-          </Switch>
+          <UserContext.Provider value={{ user }}>
+            <Navigation />
+            <Switch>
+              {routes.map((route, index) => {
+                return route.isPrivate ? (
+                  <PrivateRoute
+                    key={`route-${index * Date.now()}`}
+                    path={route.path}
+                    exact={route.exact}
+                    render={route.render}
+                  />
+                ) : (
+                  <Route
+                    key={`route-${index * Date.now()}`}
+                    path={route.path}
+                    exact={route.exact}
+                    render={route.render}
+                  />
+                );
+              })}
+              ;
+            </Switch>
+          </UserContext.Provider>
         </div>
       </Router>
     </Fragment>
