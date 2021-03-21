8fcbc43a14dccd26b0dd6e33b7c0b124fcf53b04
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-21 15:43:30 -0700

    usercontext success

--- client/src/pages/PrivateRoute.js
+++ client/src/pages/PrivateRoute.js
@@ -1,47 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Route, Redirect, useLocation } from 'react-router-dom';
-import { getAccessToken } from '../accessToken';
+import UserContext from '../context/user-context';
 
 const PrivateRoute = (props) => {
   const { key, path, exact, render } = props;
-  const [isLoading, setIsLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { user, isLoading } = useContext(UserContext);
   const location = useLocation();
-
-  const isVerified = async () => {
-    try {
-      const accessToken = getAccessToken();
-      const response = await fetch('http://localhost:5000/auth/is-verified', {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          Authorization: `bearer ${accessToken}`,
-        },
-      });
-      const parseRes = await response.json();
-      if (parseRes) {
-        setIsAuthenticated(true);
-      }
-      setIsLoading(false);
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log(err.message);
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    isVerified();
-  }, []);
 
   if (isLoading) {
     return <div>Loading... </div>;
   }
 
-  if (isAuthenticated) {
+  if (user) {
     return <Route key={key} path={path} exact={exact} render={render} />;
   }
-  // eslint-disable-next-line no-console
+
   return (
     <Redirect to={{ pathname: '/login', state: { from: location.pathname } }} />
   );
