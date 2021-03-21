aea15510c976c0fa65a435cf33fe74fdb6bb6c73
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-21 15:42:40 -0700

    setuser context init

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,33 @@
-import React, { Fragment, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { setAccessToken } from '../accessToken';
+import React, { Fragment, useState, useContext } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { setAccessToken, getAccessToken } from '../accessToken';
+import UserContext from '../context/user-context';
 
 const Login = () => {
+  const { setUser } = useContext(UserContext);
   const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { state } = useLocation();
+
+  const setUserContext = async () => {
+    try {
+      const accessToken = getAccessToken();
+      const response = await fetch('http://localhost:5000/dashboard', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          Authorization: `bearer ${accessToken}`,
+        },
+      });
+      const parseRes = await response.json();
+      setUser(parseRes);
+      history.push(state?.from || '/dashboard');
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err.message);
+    }
+  };
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
@@ -23,8 +45,8 @@ const Login = () => {
       // eslint-disable-next-line no-console
       if (parseRes) {
         setAccessToken(parseRes.accessToken);
+        await setUserContext();
       }
-      history.push('/dashboard');
     } catch (err) {
       // eslint-disable-next-line no-console
       console.error(err.message);
