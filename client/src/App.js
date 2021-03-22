38d76a0e916417478f1aaf4b350db9e7bcac1246
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-22 12:34:18 -0700

    loader

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Routes from './components/Routes';
+import { setAccessToken } from './accessToken';
+import Loader from './components/Loader';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const renewAccessToken = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/renewAccessToken', {
+        method: 'POST',
+        credentials: 'include',
+      });
+      const parseRes = await response.json();
+      if (parseRes) {
+        setAccessToken(parseRes.accessToken);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      // eslint-disable-next-line no-console
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    renewAccessToken();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return <Routes />;
+};
+
+export default App;
