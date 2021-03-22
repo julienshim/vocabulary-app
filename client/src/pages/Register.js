3d47326364f7baeea45083fc8770e02b33cfe5cd
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-22 09:11:32 -0700

    use auth conversion

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,39 +1,24 @@
-import React, { Fragment, useState, useContext } from 'react';
-import UserContext from '../context/user-context';
-import { setAccessToken } from '../accessToken';
+import React, { Fragment, useState } from 'react';
+import useAuth from '../hooks/useAuth';
 
 const Register = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { registerUser, error } = useAuth();
 
   // eslint-disable-next-line no-unused-vars
-  const { setUser } = useContext(UserContext);
-
   const onSubmitForm = async (e) => {
     e.preventDefault();
-    try {
-      const body = { email, password, name };
-      const response = await fetch('http://localhost:5000/auth/register', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(body),
-      });
-      const parseRes = await response.json();
-      setAccessToken(parseRes.accessToken);
-      // eslint-disable-next-line no-console
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error(err.message);
-    }
+    const body = { email, name, username, password };
+    registerUser(body);
   };
 
   return (
     <Fragment>
       <h1 className="text-center my-5">Register</h1>
+      <div className="invalid-feedback">{error && error.message}</div>
       <form onSubmit={onSubmitForm}>
         <input
           type="text"
@@ -44,19 +29,27 @@ const Register = () => {
           className="form-control my-3"
         />
         <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="form-control my-3"
-        />
-        <input
           type="text"
           name="name"
           value={name}
           placeholder="name"
           onChange={(e) => setName(e.target.value)}
+          className="form-control my-3"
+        />
+        <input
+          type="text"
+          name="username"
+          value={username}
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+          className="form-control my-3"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
           className="form-control my-3"
         />
         <button type="submit" className="btn btn-success btn-block">
