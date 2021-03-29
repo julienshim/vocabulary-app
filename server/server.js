31a45bb873ab8292c03b7694c66eb1aacff18c9e
julienshim <25723428+julienshim@users.noreply.github.com>
2021-03-28 21:11:11 -0700

    auth dash refresh exp revoke, cookie parser

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 // const keys = require('./config/keys');
 
 require('dotenv').config();
@@ -8,13 +10,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 const cardsRouter = require('./routes/cards');
+const authRouter = require('./routes/auth');
+const dashboardRouter = require('./routes/dashboard');
+const refreshRouter = require('./routes/renew');
+const revokeRouter = require('./routes/revoke');
 
 app.use('/cards', cardsRouter);
+app.use('/auth', authRouter);
+app.use('/dashboard', dashboardRouter);
+app.use('/renewAccessToken', refreshRouter);
+app.use('/revokeRefreshTokensForUsers', revokeRouter);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
