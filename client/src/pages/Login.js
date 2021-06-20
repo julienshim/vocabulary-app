75e910e250e223ead12ecd288328925ce7f933d3
julienshim <25723428+julienshim@users.noreply.github.com>
2021-06-20 13:18:01 -0700

    initial feedback

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,12 +5,18 @@ import useAuth from '../hooks/useAuth';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const { loginUser } = useAuth();
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
     const body = { email, password };
-    await loginUser(body);
+    const user = await loginUser(body);
+    // eslint-disable-next-line no-console
+    if (!user) {
+      // eslint-disable-next-line no-console
+      setError('Invalid email address or password. Please try again.');
+    }
   };
 
   return (
@@ -22,22 +28,37 @@ const Login = () => {
           name="email"
           value={email}
           placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            if (error) {
+              setError('');
+            }
+            setEmail(e.target.value);
+          }}
           className="form-control my-3"
+          required
         />
         <input
           type="password"
           name="password"
           value={password}
           placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            if (error) {
+              setError('');
+            }
+            setPassword(e.target.value);
+          }}
           className="form-control my-3"
+          required
         />
         <button type="submit" className="btn btn-success btn-block">
           Login
         </button>
       </form>
       <Link to="/register">Register</Link>
+      <div>
+        <span>{error}</span>
+      </div>
     </Fragment>
   );
 };
