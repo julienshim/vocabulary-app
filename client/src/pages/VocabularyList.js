4ada897ef2e7ea159039d61d3e8ca3f5d9b52a7c
julienshim <25723428+julienshim@users.noreply.github.com>
2021-06-22 08:47:38 -0700

    why func. convert to arrow

--- client/src/pages/VocabularyList.js
+++ client/src/pages/VocabularyList.js
@@ -5,12 +5,14 @@ import AddCardForm from '../components/AddCardForm';
 import CardsList from '../components/CardsList';
 import PageNavigation from '../components/PageNavigation';
 import Loader from '../components/Loader';
+
+// hooks
 import useGetCards from '../hooks/useGetCards';
 
 // context
 import CardsContext from '../context/cards-context';
 
-function VocabularyList() {
+const VocabularyList = () => {
   const {
     cards,
     cardsDispatch,
@@ -46,6 +48,6 @@ function VocabularyList() {
       <PageNavigation />
     </CardsContext.Provider>
   );
-}
+};
 
 export default VocabularyList;
