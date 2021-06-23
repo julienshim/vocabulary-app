4ac62266d171f5755f8698084352a70a78476431
julienshim <25723428+julienshim@users.noreply.github.com>
2021-06-23 11:43:43 -0700

    rename

---
+++ client/src/components/ListVocabularyCards.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useContext } from 'react';
+import Card from './Card';
+import CardsContext from '../context/cards-context';
+
+const ListVocabularyCards = () => {
+  const { cards } = useContext(CardsContext);
+  return (
+    <Fragment>
+      <h1>Vocabulary List</h1>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th />
+            <th className="col-width" scope="col">
+              Korean
+            </th>
+            <th className="col-width" scope="col">
+              English
+            </th>
+            <th className="col-width" scope="col">
+              Hanja
+            </th>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th />
+            {/* <th className="col-width" scope="col">
+              onMaster
+            </th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {cards
+            ? cards.map((card) => (
+                <Card key={`card-${card.card_id}`} card={card} />
+              ))
+            : 'No cards found'}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListVocabularyCards;
