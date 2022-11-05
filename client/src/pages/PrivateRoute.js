72a44431cca4db6ea5a30e8adeaa8df0b4f08dfe
julienshim <25723428+julienshim@users.noreply.github.com>
2022-11-04 23:14:28 -0700

    rr v6

--- client/src/pages/PrivateRoute.js
+++ client/src/pages/PrivateRoute.js
@@ -1,28 +1,37 @@
 import React, { useContext } from 'react';
-import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import UserContext from '../context/user-context';
 import Loader from '../components/Loader';
 
 const PrivateRoute = (props) => {
   // const { key, path, exact, render } = props;
+  const { children } = props;
   const { user, isLoading } = useContext(UserContext);
   const location = useLocation();
+  const navigate = useNavigate();
 
   if (isLoading) {
     return <Loader />;
   }
 
   if (user) {
-    <Outlet />;
+    return children;
     // return <Route key={key} path={path} exact={exact} render={render} />;
   }
 
-  return (
-    <Navigate
-      replace
-      to={{ pathname: '/login', state: { from: location.pathname } }}
-    />
-  );
+  // const { from } = location.state};
+  navigate('/login', {
+    replace: true,
+    state: {
+      from: location.pathname,
+    },
+  });
+  // return (
+  //   <Navigate
+  //     replace
+  //     to={{ pathname: '/login', state: { from: location.pathname } }}
+  //   />
+  // );
 };
 
 export default PrivateRoute;
